1dd1cd81b4caef5cd414ca78cdbe954fb80ed634
Lu-GN <113623624+Lu-GN@users.noreply.github.com>
2024-04-18 19:31:08 +0200

    added elements to the welcome screen

--- js/welcomescreen.js
+++ js/welcomescreen.js
@@ -13,3 +13,14 @@ function loadassets() {
     game.load.image('about', 'assets/about.png');
     game.load.image('instructions', 'assets/instructions.png');
 }
+
+let buttonStart, buttonAbout, buttonInstructions;
+
+function display() {
+    buttonStart = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2 - 100, 'start', startGame);
+    buttonStart.anchor.setTo(0.5, 0.5);
+    buttonAbout = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2, 'about', aboutGame);
+    buttonAbout.anchor.setTo(0.5, 0.5);
+    buttonInstructions = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2 + 100, 'instructions', instructionsGame);
+    buttonInstructions.anchor.setTo(0.5, 0.5);
+}
