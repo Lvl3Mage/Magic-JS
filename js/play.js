a5fe178e0088ea040b7bcd4a3d9dbdbb01f979a2
SaulPachecoTrilles <saulpachecotrilles@gmail.com>
2024-04-18 19:32:29 +0200

    First commit score + dificulty lv text

--- js/play.js
+++ js/play.js
@@ -1,3 +1,15 @@
+
+//  3. HUDin-play screens (both for part A & B if done):
+let score;
+let scoreText;
+let level;
+let levelText;
+let lives;
+let livesBar;
+let livesScore;
+//! 3.b, 3.c
+
+
 let playState = {
     preload: preloadPlay,
     create: createPlay,
@@ -5,13 +17,39 @@ let playState = {
 };
 
 function preloadPlay() {
-    
+
 }
 
 function createPlay() {
-    
+    score = 0;
+    level = 1;
+    lives = 3;
+
+    createHUD();
 }
 
 function updatePlay() {
-    
+
+}
+
+
+
+function createHUD() {
+    let scoreX = 1;
+    let levelX = game.world.width / 2; //Ya veremos como escala
+    let allY = game.world.height - 25;
+    let styleHUD = {fontSize: '18px', fill: '#FFFFFF'};
+    scoreText = game.add.text(scoreX,allY,'Score: '+score,styleHUD);
+    levelText = game.add.text(levelX,allY,'Level: '+level,styleHUD);
+    levelText.anchor.setTo(0.5, 0);
+}
+
+//NOT IMPLEMENTED
+function updateLife(num){
+    let livesBar = document.getElementById("livesScore");
+    livesScore += num;
+    console.log(livesScore);
+    let porcentajeVida = (livesScore / lives) * 100; // 3 vidas en total
+    livesBar.style.width = porcentajeVida + "%";
+    // livesBar.anchor.setTo(1, 0);
 }
