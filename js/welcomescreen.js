4c0cf16246a7e668aededde1b20aee2112939cc5
Lu-GN <113623624+Lu-GN@users.noreply.github.com>
2024-04-19 19:22:44 +0200

    Made the welcome screen visible and (relatively) usable

--- js/welcomescreen.js
+++ js/welcomescreen.js
@@ -4,23 +4,37 @@ let welcomeState = {
     create: display
 };
 
-function loadassets() {
+function loadassets() { //THESE ASSETS ARE CURRENTLY TEMPORARY AND WILL BE DRAWN LATER
     // Load the background image
-    game.load.image('background', 'assets/background.png');
+    game.load.image('background', 'assets/imgs/stars.png');
     // Load the necessary buttons
-    //Those being the start button, about and instructions
-    game.load.image('start', 'assets/start.png');
-    game.load.image('about', 'assets/about.png');
-    game.load.image('instructions', 'assets/instructions.png');
+    //Those being the start button, about and configutration
+    game.load.image('start', 'assets/imgs/TEMPORARY IMAGES/playButton.png');
+    game.load.image('about', 'assets/imgs/TEMPORARY IMAGES/aboutButton.png');
+    game.load.image('config', 'assets/imgs/TEMPORARY IMAGES/configButton.png');
 }
 
-let buttonStart, buttonAbout, buttonInstructions;
+let buttonStart, buttonAbout, buttonConfig;
 
 function display() {
-    buttonStart = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2 - 100, 'start', startGame);
+    game.input.enabled = true;
+    game.add.image(0,0, 'background');
+
+    buttonStart = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2 - 100, 'start', onStartButtonPressed);
     buttonStart.anchor.setTo(0.5, 0.5);
-    buttonAbout = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2, 'about', aboutGame);
+    buttonAbout = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2, 'about', onAboutButtonPressed);
     buttonAbout.anchor.setTo(0.5, 0.5);
-    buttonInstructions = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2 + 100, 'instructions', instructionsGame);
-    buttonInstructions.anchor.setTo(0.5, 0.5);
+    buttonConfig = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2 + 100, 'config', onConfigButtonPressed);
+    buttonConfig.anchor.setTo(0.5, 0.5);
+}
+
+
+function onStartButtonPressed() {
+    game.state.start('start');
+}
+function onAboutButtonPressed() {
+    game.state.start('about');
+}
+function onConfigButtonPressed() {
+    game.state.start('config');
 }
