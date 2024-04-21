10efc99466a9233ff849bdee16a720b0df224a7a
Lvl3Mage <lvlthreemage@gmail.com>
2024-04-21 18:36:46 +0200

    HAND

--- js/components/Player.js
+++ js/components/Player.js
@@ -6,6 +6,7 @@ class Player {
 	}
 	Load(){
 		game.load.image('main-character', 'assets/imgs/main-character.png');
+		game.load.image('hand', 'assets/imgs/hand-placeholder.png');
 	}
 	Create(){
 		this.sprite = game.add.sprite(0, 0, 'main-character');
@@ -14,6 +15,9 @@ class Player {
 
 		this.sprite.anchor.setTo(0.5, 1);
 
+		this.handSprite = game.add.sprite(0, 0, 'hand');
+		this.handSprite.anchor.setTo(0.5, 0.5);
+
 		//Input handling
 		this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 		this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
@@ -21,13 +25,16 @@ class Player {
 		this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
 
 
-		//Parameters
+		//Player Parameters
 		this.maxVelocity = 300;
 		this.accelerationFactor = 0.1;
-		this.maxTilt = 15;
+		this.maxTilt = 25;
 		this.bopFreq = 15;
-		this.maxTiltBopAmp = 7;
-		this.bopAmp = 0.1;
+		this.maxTiltBopAmp = 15;
+		this.bopAmp = 0.3;
+
+		//Hand Parameters
+		this.handDistance = 17;
 	}
 	GetInputAxis(){
 		let axis = new Vector2(0,0);
@@ -71,5 +78,22 @@ class Player {
 		let dif = Vector2.Lerp(camCenterPos, playerPos, 0.1).Sub(camCenterPos);
 		game.camera.x += dif.x;
 		game.camera.y += dif.y;
+
+		let playerSpriteCenter = new Vector2(this.sprite.x, this.sprite.y);
+		playerSpriteCenter = playerSpriteCenter.Add(CoordUtils.InverseTransformPoint(Vector2.down.Scale(this.sprite.height*(0.5 + bop - 1)), Vector2.zero, Mathf.Deg2Rad(-this.sprite.angle)))
+
+		let worldCursor = CoordUtils.ScreenSpaceToWorldSpace(new Vector2(game.input.mousePointer.x, game.input.mousePointer.y));
+		let localCursor = CoordUtils.InverseTransformPoint(worldCursor, playerSpriteCenter, Mathf.Deg2Rad(this.sprite.angle));
+		console.log(localCursor);
+		let handPos = CoordUtils.TransformPoint(localCursor.Normalized().Scale(this.handDistance), playerSpriteCenter, Mathf.Deg2Rad(this.sprite.angle));
+		this.handSprite.x = handPos.x;
+		this.handSprite.y = handPos.y;
+
+
+		let handToMouse = worldCursor.Sub(handPos);
+		let handAngle = Mathf.Rad2Deg(Math.atan2(handToMouse.y, handToMouse.x));
+		this.handSprite.angle = handAngle;
+		console.log(localCursor.Normalized());
+		console.log(game.input.mousePointer.x.toString() + " " + game.input.mousePointer.y.toString());
 	}
 }
