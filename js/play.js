c84398e0fc9983d8a33206ad5dd09fe1ec1beb30
SaulPachecoTrilles <saulpachecotrilles@gmail.com>
2024-04-23 01:42:50 +0200

    healthBar done and and fixedToCamera functionality

--- js/play.js
+++ js/play.js
@@ -6,7 +6,8 @@ let level;
 let levelText;
 let lives;
 let livesBar;
-let livesScore;
+let livesBarOutline;
+let livesScore; // esto solo irá de 0 a 1
 //! 3.b, 3.c
 
 
@@ -17,13 +18,15 @@ let playState = {
 };
 
 function preloadPlay() {
-
+    game.load.image('healthbar_outline', 'assets/imgs/healthbar_outline.png');
+    game.load.image('healthbar_mask_red', 'assets/imgs/healthbar_mask_red.png');
 }
 
 function createPlay() {
     score = 0;
     level = 1;
     lives = 3;
+    livesScore = 1;
 
     createHUD();
 }
@@ -35,21 +38,34 @@ function updatePlay() {
 
 
 function createHUD() {
-    let scoreX = 1;
+    let scoreX = game.world.width - 10;
     let levelX = game.world.width / 2; //Ya veremos como escala
-    let allY = game.world.height - 25;
+    let healthX= 10;
+    let allY = 10; //game.world.height
     let styleHUD = {fontSize: '18px', fill: '#FFFFFF'};
+
     scoreText = game.add.text(scoreX,allY,'Score: '+score,styleHUD);
+    scoreText.anchor.setTo(1, 0);
+    scoreText.fixedToCamera = true;
+    scoreText.cameraOffset = new Phaser.Point(scoreX,allY);
+
     levelText = game.add.text(levelX,allY,'Level: '+level,styleHUD);
     levelText.anchor.setTo(0.5, 0);
-}
+    levelText.fixedToCamera = true;
+    levelText.cameraOffset = new Phaser.Point(levelX,allY);
 
-//NOT IMPLEMENTED
-function updateLife(num){
-    let livesBar = document.getElementById("livesScore");
-    livesScore += num;
-    console.log(livesScore);
-    let porcentajeVida = (livesScore / lives) * 100; // 3 vidas en total
-    livesBar.style.width = porcentajeVida + "%";
-    // livesBar.anchor.setTo(1, 0);
+    //healthbar
+    livesBar = game.add.sprite(healthX, allY, 'healthbar_mask_red');
+    livesBar.fixedToCamera = true;
+    livesBar.cameraOffset = new Phaser.Point(healthX,allY);
+    let barMask = game.add.graphics(0,0);
+    barMask.beginFill(0xfff);
+    barMask.drawRect(healthX, allY, livesBar.width * livesScore, livesBar.height);
+    barMask.endFill();
+    livesBar.mask = barMask;
+    console.log(livesBar);
+
+    livesBarOutline = game.add.sprite(healthX, allY, 'healthbar_outline');
+    livesBarOutline.fixedToCamera = true;
+    livesBarOutline.cameraOffset = new Phaser.Point(healthX,allY);
 }
