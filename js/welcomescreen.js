078e86b4120c1522c9f599d6242876fc20178f70
Lu-GN <113623624+Lu-GN@users.noreply.github.com>
2024-04-22 09:30:55 +0200

    Welcome screen is now functional
    need to work on the rest

--- js/welcomescreen.js
+++ js/welcomescreen.js
@@ -20,12 +20,30 @@ function display() {
     game.input.enabled = true;
     game.add.image(0,0, 'background');
 
+
+    //Our game title
+    let gameTitle = 'Epic Temporary Game Title!';
+    let styleTitle = { 
+        font: 'bold 60px Arial', 
+        fill: '#ffffff', 
+        lign: 'center' };
+    game.add.text(GAME_STAGE_WIDTH/4, GAME_STAGE_HEIGHT/6, gameTitle, styleTitle);
+
+    //Buttons corresponding to the start, about and configuration respectively
     buttonStart = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2 - 100, 'start', onStartButtonPressed);
     buttonStart.anchor.setTo(0.5, 0.5);
+    buttonStart.onInputOver.add(onButtonHover, this); // Add hover event listener
+    buttonStart.onInputOut.add(onButtonOut, this); // Add hover out event listener
+
     buttonAbout = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2, 'about', onAboutButtonPressed);
     buttonAbout.anchor.setTo(0.5, 0.5);
+    buttonAbout.onInputOver.add(onButtonHover, this); // Add hover event listener
+    buttonAbout.onInputOut.add(onButtonOut, this); // Add hover out event listener
+
     buttonConfig = game.add.button(GAME_STAGE_WIDTH / 2, GAME_STAGE_HEIGHT / 2 + 100, 'config', onConfigButtonPressed);
     buttonConfig.anchor.setTo(0.5, 0.5);
+    buttonConfig.onInputOver.add(onButtonHover, this); // Add hover event listener
+    buttonConfig.onInputOut.add(onButtonOut, this); // Add hover out event listener
 }
 
 
@@ -37,4 +55,13 @@ function onAboutButtonPressed() {
 }
 function onConfigButtonPressed() {
     game.state.start('config');
+}
+
+function onButtonHover(button) {
+    game.add.tween(button.scale).to({ x: 1.1, y: 1.1 }, 400, Phaser.Easing.Linear.None, true, 0, -1, true);
+}
+
+function onButtonOut(button) {
+    game.tweens.removeFrom(button.scale);
+    button.scale.setTo(1, 1);
 }
