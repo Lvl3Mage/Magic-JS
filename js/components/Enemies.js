99eae38ac925a22db738713db972eb894a4afdec
Lu-GN <113623624+Lu-GN@users.noreply.github.com>
2024-05-02 15:24:52 +0200

    Did some major changes, however the enemies aren't yet all functional

--- js/components/Enemies.js
+++ js/components/Enemies.js
@@ -10,6 +10,11 @@ class Enemy {
         eventSystem.Subscribe("preload", this.Load.bind(this));
         eventSystem.Subscribe("create", this.Create.bind(this));
         eventSystem.Subscribe("update", this.Update.bind(this));
+
+        this.isAggressive = false;
+
+        this.moveTimer = 0;
+        this.moveDelay = 1000; //At least 1 second delay, remembering this is in milliseconds
     }
 
     Load() {
@@ -19,15 +24,44 @@ class Enemy {
     }
 
     Create() {
-        //Creating the enemy here
+        //Creating one enemy
         const spawnX = Math.random() * (game.world.width - ENEMY_X_OFFSET) + ENEMY_X_OFFSET;
         const spawnY = Math.random() * (game.world.height - ENEMY_Y_OFFSET) + ENEMY_Y_OFFSET;
         
-        const enemy = game.add.sprite(spawnX, spawnY, 'enemySprite');
-        enemy.anchor.setTo(0.5, 0.5);
+        this.sprite = game.add.sprite(spawnX, spawnY, 'enemySprite');
+        this.sprite.anchor.setTo(0.5, 0.5);
     }
 
     Update() {
         //The enemy behaviour will go here
+        if (this != this.isAggressive) {
+            this.moveAround();
+        }
+        else {
+            this.attack();
+        }
+    }
+
+    moveAround() {
+        //I'll make the enemy follow a random path
+        //I need a timer so that the time intervals are also randomised
+        this.moveTimer += game.time.elapsed; //This is in milliseconds
+
+        if(this.moveTimer >= this.moveDelay){
+            const randomAngle = Math.random() * Math.PI * 2; //Selects a random number between 0 and 1 and multiplies by 2PI to get an angle
+
+            const newX = this.sprite.x + Math.cos(randomAngle) *  ENEMY_VELOCITY;
+            const newY = this.sprite.y + Math.sin(randomAngle) *  ENEMY_VELOCITY;
+
+            game.physics.arcade.moveToObject(this.sprite, { x: newX, y: newY }, ENEMY_VELOCITY);
+
+            //Once it has moved to the new position, we reset the timer
+            this.moveTimer = 0;
+        }
+    }
+
+    attack() {
+        //Check if the player is within the attack distance
+
     }
 }
--- js/play.js
+++ js/play.js
@@ -12,6 +12,10 @@ let livesBarOutline;
 let livesScore; // esto solo irá de 0 a 1
 //! 3.b, 3.c
 
+let maxEnemies = 10;
+let spawnDelay = 3000;
+let enemiesSpawned = 0;
+
 
 let playState = {
 	preload: preloadPlay,
@@ -25,6 +29,8 @@ const player = new Player(eventSystem);
 function preloadPlay() {
 	game.load.image('healthbar_outline', 'assets/imgs/healthbar_outline.png');
 	game.load.image('healthbar_mask_red', 'assets/imgs/healthbar_mask_red.png');
+
+	game.load.image('enemySprite', 'assets/imgs/PLACEHOLDERS/default_cube.png');
 	
     eventSystem.CallEvent("preload", []);
 }
@@ -42,6 +48,8 @@ function createPlay() {
 	game.camera.roundPx = false;
 	eventSystem.CallEvent("create", []);
 	
+	//I'll start spawning enemies here
+	game.time.events.add(spawnDelay, spawnEnemies, this);
 }
 
 function updatePlay() {
@@ -80,4 +88,16 @@ function createHUD() {
 	livesBarOutline = game.add.sprite(healthX, allY, 'healthbar_outline');
 	livesBarOutline.fixedToCamera = true;
 	livesBarOutline.cameraOffset = new Phaser.Point(healthX,allY);
+}
+
+function spawnEnemies() {
+	//Checking if the number of enemies has been surpassed
+	if(enemiesSpawned < maxEnemies){
+		const newEnemy = new Enemy(eventSystem);
+		newEnemy.Create();
+		enemiesSpawned++;
+
+		//Making time for the next enemy to spawn
+		game.time.events.add(spawnDelay, spawnEnemies, this);
+	}
 }
