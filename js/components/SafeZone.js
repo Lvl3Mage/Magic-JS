d1e7a20989a1d91c31f68fa15187bbd6a62f595d
Lu-GN <113623624+Lu-GN@users.noreply.github.com>
2024-05-15 18:26:52 +0200

    GUH

--- js/components/SafeZone.js
+++ js/components/SafeZone.js
@@ -2,9 +2,12 @@ class SafeZone extends Component {
 	constructor(eventSystem, position, dimensions) {
 		super(eventSystem);
 		eventSystem.Subscribe("scene-update", this.Update, this);
+		
 		this.sprite = game.add.sprite(position.x,position.y);
         this.sprite.getParentComponent = () => this;
+
         game.physics.p2.enable(this.sprite,true);
+
 		this.body = this.sprite.body;
 		this.body.fixedRotation = true;
 		console.log(this.sprite);
@@ -15,12 +18,21 @@ class SafeZone extends Component {
         this.body.collides(sceneData.collisionGroups.enemies);
         this.body.kinematic = true;
         console.log(this.body);
+	}
 
-	}
 	Update(){
+		const playerBounds = sceneData.player.sprite.getBounds();
+		const safeZoneBounds = this.sprite.getBounds();
+
+		if (Phaser.Rectangle.intersects(playerBounds, safeZoneBounds)){
+			console.log("Player entered safe zone");
+		} else {
+			console.log("Player left safe zone");
+		}
 	}
+
 	onPlayerCollision(){
-		sceneData.HUD.createText(0.5, 1, sceneData.HUD.centerScreen, sceneData.HUD.allY, `Tiempo: ` + 0);
-		console.log("Player entered safe zone");
+		//sceneData.HUD.createText(0.5, 1, sceneData.HUD.centerScreen, sceneData.HUD.allY, `Tiempo: ` + 0);
+		//console.log("Player entered safe zone");
 	}
 }
