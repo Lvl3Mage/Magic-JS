60743c7f4a1e7100631465cad824c19ac1c253b0
Lvl3Mage <lvlthreemage@gmail.com>
2024-05-17 02:00:14 +0200

    Removed physics from collectible

--- js/components/Collectible.js
+++ js/components/Collectible.js
@@ -6,17 +6,13 @@ class Collectible {
         this.sprite.scale.setTo(0.3, 0.3);
         this.sprite.getParentComponent = () => this;
         this.config = collectibleConfig;
-
-        game.physics.p2.enable(this.sprite, false);
-        this.body = this.sprite.body;
-        this.body.setCollisionGroup(sceneData.collisionGroups.collectables);
-        this.body.collides(sceneData.collisionGroups.player, collectibleConfig.onPlayerCollision.bind(this), this);
-        this.body.getParentComponent = () => this;
+        this.collectDistance = 50;
         this.bopPhaseOffset = Math.random();
 
 
         this.attractionDistance = 200;
-        this.maxAttractionVelocity = 1000;
+        this.maxAttractionVelocity = 2000;
+        this.velocity = new Vector2(0,0);
     }
 
     Update() {
@@ -25,6 +21,11 @@ class Collectible {
         this.sprite.anchor.setTo(0.5, Mathf.TransformRange(-1,1 , 0.5,1, bop));
 
         this.MoveToPlayer();
+        
+        const distanceToPLayer = sceneData.player.GetPosition().Sub(new Vector2(this.sprite.centerX, this.sprite.centerY)).Length();
+        if(distanceToPLayer < 50){
+            this.config.onPlayerCollision.call(this);
+        }
     }
     MoveToPlayer(){
         const delta = sceneData.player.GetPosition().Sub(new Vector2(this.sprite.centerX, this.sprite.centerY));
@@ -32,11 +33,9 @@ class Collectible {
         let velocityMagnitude = Mathf.TransformRange(0,this.attractionDistance, this.maxAttractionVelocity, 0, distance);
         velocityMagnitude = Mathf.Clamp(velocityMagnitude,0, this.maxAttractionVelocity);
         const dir = delta.Normalized();
-        let currentVel = new Vector2(this.body.velocity.x, this.body.velocity.y);
-        currentVel = Vector2.Lerp(currentVel, dir.Scale(velocityMagnitude), 0.1);
-        console.log(dir.Scale(velocityMagnitude));
-        this.body.velocity.x = currentVel.x;
-        this.body.velocity.y = currentVel.y;
+        this.velocity = Vector2.Lerp(this.velocity, dir.Scale(velocityMagnitude), 0.1);
+        this.sprite.x += this.velocity.x * game.time.elapsed * 0.001;
+        this.sprite.y += this.velocity.y * game.time.elapsed * 0.001;
 
     }
 }
