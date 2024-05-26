939538544d3f6d58014654dd609c69c34f72a642
SaulPachecoTrilles <78241212+Salix8@users.noreply.github.com>
2024-05-26 16:21:16 +0200

    Perfecto hasta aqui funciona la tienda y todo

--- js/configure.js
+++ js/configure.js
@@ -44,7 +44,6 @@ function doConfig() {
 
 function onButtonPressed(button) {
     if (button.name === 'btnEasy') {
-<<<<<<< Updated upstream
         // Update the parameters for each control
         console.log("hello! You pressed the Easy button");
         goHome();
@@ -57,20 +56,6 @@ function onButtonPressed(button) {
     } else if (button.name === 'btnNgtm') {
         // Update the parameters for each control
         console.log("hello! You pressed the Nightmare button");
-=======
-        difficulty = 'easy';
-        difficultyText = 'Easy';
-        goHome();
-        
-    } else if (button.name === 'btnAvg') {
-        difficulty = 'medium';
-        difficultyText = 'Medium';
-        goHome();
-        
-    } else if (button.name === 'btnNgtm') {
-        difficulty = 'nightmare';
-        difficultyText = 'Nightmare';
->>>>>>> Stashed changes
         goHome();
     }
 }
